6dba192e92c43060a6dc7661a1bdea9a97cd4dc8
ykkd <ykkd221dev@gmail.com>
2024-09-06 18:50:10 +0900

    fix: minor improvements

--- 第6章 関数とクロージャ.playground/Contents.swift
+++ 第6章 関数とクロージャ.playground/Contents.swift
@@ -15,3 +15,16 @@ import Foundation
 // 2つの整数の差を計算するクロージャ「subtract」を定義してください。ただし、最初の引数をfirstNumber,2つ目の引数をsecondNumberとして定義し、firstNumberからsecoundNumberを引くクロージャとして定義してください。
 
 // 変数「difference」を宣言し、「subtract」クロージャを使って9から3を引いた差を代入してください。
+
+/*
+ 引数なし、返り値なしのクロージャを引数に取る関数「greetAndDoSomething」を定義しなさい。引数名は「completion」にしなさい
+ 関数内では以下処理を順に行いなさい。
+ - 前述のgreet関数を実行（引数には"John"を指定しなさい）し、結果を出力する
+ - completionを実行しなさい
+ */
+
+/*
+ greetAndDoSomething関数を用いて、以下が出力しなさい
+ "Hello, John"
+ "What's up?"
+ */
--- 第8章 型の種類.playground/Contents.swift
+++ 第8章 型の種類.playground/Contents.swift
@@ -12,33 +12,35 @@ import Foundation
 
 // 変数「person2」を宣言し、「person1」を代入してください。次に、「person2」のnameの値を"Jane"に変更してください。最後に「person1」のnameの値を出力してください。
 
+// 構造体「Point」とクラス「Person」を用いた問題からわかる、クラスと構造体の違いについて簡潔に記載してください。
+
 /*
  新しい列挙型「Fruit」を定義し、その中に「apple」「banana」「cherry」というケースを追加してください。
  それぞれのケースに対応する文字列型のrawValueを設定してください。rawValueはcase名を大文字始まりにしたものにしてください。
- さらに、Bool型の計算型プロパティ「isRed」を定義してください。「isRed」は赤い色の果物の場合のみ、trueを、その他の場合はfalseを返してください。
+ さらに、Bool型の計算型プロパティ「isTropical」を定義してください。
+ 「isTropical」は熱帯で育つ果物か否かを意味するフラグです。バナナの場合のみ、trueを、その他の場合はfalseを返してください。
  */
+
 
 /*
  定数「apple」に列挙型Fruitのインスタンス case appleを代入してください。
- appleが赤い色の果物である場合は、「赤い色の果物です」と、そうではない場合は「赤い色の果物ではありません」と出力してください
+ appleが熱帯で育つ果物である場合は、「熱帯で育つ果物です」と、そうではない場合は「熱帯で育つ果物ではありません」と出力してください
  */
 
-
 /*
- 方角を表現した列挙型「Direction」を定義し、その中に「north」「south」「east」「west」というケースを追加してください。
- ただし、northは1,westは4になるようにrawValueを設定してください。
+ 方角を表現した列挙型「Direction」を定義し、その中に「north」「south」「east」「west」というケースを順に追加してください。
+ ただし、northは1,southは2,eastは3,westは4になるようにrawValueを設定してください。
  さらに、rawValueを使用して初期化できるイニシャライザを追加してください。ただし、存在するcaseのrawValueに対応する値以外が渡された場合は、nilを返却してください。
  */
 
-
 /*
  新しい列挙型「ShapeType」を定義し、その中に「circle」「rectangle」「triangle」というケースを追加してください。
- 以下の通り、それぞれのケースに対応する連想値を持たせてください。
- circle（円）は半径を表すDouble型、rectangle（四角形）は幅と高さを表すDouble型の変数二つ、triangle（三角形）は底辺と高さを表すDouble型の変数二つ。
+ 以下の通り、それぞれのケースに対応する連想値を持たせてください。ただし、連想値には任意の名前を持たせてください。
+ circle（円）は半径(radius)を表すDouble型、rectangle（四角形）は幅(width)と高さ(height)を表すDouble型の変数二つ、triangle（三角形）は底辺(base)と高さ(height)を表すDouble型の変数二つ。
 
  列挙型「ShapeType」に、各形状の面積を計算してDouble型の値を返すメソッド「area」を追加してください。
  ただし、円周率にはDouble.piを用いてください。
  */
 
-
 // 列挙型「ShapeType」を活用して、半径の値が34の円の面積を出力してください
+
